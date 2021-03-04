2cd2cd9b2aeafd7507fb1f52f65699ae33af2891
Your Namemohabib02 <you@example.comk.habib02@gmail.com>
2021-03-05 00:54:56 +0100

    migrated and created database tables and admin panel layout

---
+++ database/migrations/2021_03_03_200917_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id');
+            $table->string('title',200);
+            $table->string('description',250);
+            $table->double('cost_price');
+            $table->double('price');
+            $table->double('unit');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
